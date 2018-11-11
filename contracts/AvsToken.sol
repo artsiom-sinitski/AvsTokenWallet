39e15db0b73e53fc66939fec1a6a83ba99e55cd6
Artsiom Sinitski <artsiom.vs@gmail.com>
2018-11-11 19:41:22 +0300

    Added owner transfer & destroy functionality

--- contracts/AvsToken.sol
+++ contracts/AvsToken.sol
@@ -4,23 +4,31 @@ import "./StandardToken.sol";
 import "./StandardBurnableToken.sol";
 
 contract AvsToken is StandardToken, StandardBurnableToken {
+
+  // STATE DATA
   address private owner;
   string private name = "AvsToken";
   string private symbol = "AvS";
   uint8 private decimals = 2;
   uint256 private INITIAL_SUPPLY = 10000;
 
+  //EVENTS
+  event OwnershipTransferred(address indexed currentOwner, address indexed newOwner);
+  
+  // MODIFIERS
   modifier onlyByOwner() {
     require(msg.sender == owner);
     _;
   }
-
+  
+  // CONSTRUCTOR
   constructor() public {
     owner = msg.sender;
 	  totalSupply_ = INITIAL_SUPPLY;
 	  balances[msg.sender] = INITIAL_SUPPLY;
   }
 
+  // SETTERS & GETTERS 
   function getName() public view returns (string) {
        return name;
   }
@@ -41,4 +49,31 @@ contract AvsToken is StandardToken, StandardBurnableToken {
         decimals = precision;
         return (precision, true);
   }
+
+  // CONTRACT FUNCTIONS
+  /**
+   * @dev Transfers the current balance to the owner and terminates the contract.
+   */
+  function destroy() public onlyByOwner {
+        selfdestruct(owner);
+  }
+
+  function destroyAndSend(address _recipient) public onlyByOwner {
+        selfdestruct(_recipient);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) public onlyByOwner {
+        _transferOwnership(_newOwner);
+  }
+
+  function _transferOwnership(address _newOwner) internal {
+       require(_newOwner != address(0));
+       emit OwnershipTransferred(owner, _newOwner);
+       owner = _newOwner;
+  }
+  
 }
